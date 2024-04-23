de37ae62493e854dec9030959e02ad60a0e1bb71
Shih-Hao <shihhao.lou@hotmail.com>
2024-04-23 18:42:10 +0800

    adjust the engine for openai

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def GPT_response(text):
         print("Sending request to OpenAI API with prompt:", text)
         # 接收回應
         response = openai.Completion.create(
-            engine="text-davinci-003",
+            engine="text-davinci",
             prompt=text,
             max_tokens=500,
             temperature=0.5
